70db2c3706857ab22d3f01a0ad1cf59174b01c28
AmitayGabay <amitaygabay1@gmail.com>
2025-05-08 18:17:27 +0300

    change .gitignore file & add event-listener to h1 in script.js

---
+++ server/script.js
@@ -0,0 +1,12 @@
+const h1 = document.querySelector("h1");
+
+h1.addEventListener("click", () => {
+    h1.style.color = "red";
+    setTimeout(() => {
+        h1.style.color = "white";
+    }, 500);
+    const p = document.createElement("p");
+    p.innerText = "Start learning Git...";
+    p.style.textAlign = "center";
+    document.body.appendChild(p);
+})
